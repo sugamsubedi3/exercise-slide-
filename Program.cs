ca6c453330ba9eb62dcde7f21c6a283f781b951d
Sugam Subedi <sugamsubedi173@gmail.com>
2024-02-06 21:15:16 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 10;
+int b = 5;
+
+if (a > b)
+{
+    // Exchange values
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+Console.WriteLine($"a: {a}, b: {b}");
---
+++ Programex.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class NumberAnalysis
+{
+    static void Main()
+    {
+        Console.Write("Enter a whole number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number > 0)
+        {
+            if (number % 2 == 0)
+                Console.WriteLine($"The number is positive and even");
+            else
+                Console.WriteLine($"The number is positive and odd");
+        }
+        else if (number < 0)
+        {
+            if (number % 2 == 0)
+                Console.WriteLine($"The number is negative and even");
+            else
+                Console.WriteLine($"The number is negative and odd");
+        }
+        else
+        {
+            Console.WriteLine($"The number is zero (and it is even)");
+        }
+    }
+}
